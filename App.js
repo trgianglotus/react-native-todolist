1e34da54f661a1b76220e6394baea4eece8d3be6
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-11-06 20:33:14 +0800

    add header

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+
+import Header from './components/Header';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: 1 },
+    { text: 'create an app', key: 2 },
+    { text: 'play on the switch', key: 3 },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <View>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => {
+              return <Text>{item.text}</Text>;
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +38,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
